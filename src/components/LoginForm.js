4448d2700d5ae1794b898c3c34344a3378430293
MorenoBell <morenobellini15@gmail.com>
2022-12-04 13:30:42 +0100

    addedd form with saved credentials

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -33,21 +33,21 @@ function LoginForm() {
   }
   return (
     <div className='bg-gradient-to-r from-slate-800 via-slate-700 to-slate-600 h-screen box-shadow px-10 py-10 place-content-center'>
-      <button className='hover:bg-emerald-600 text-white text-xl border-slate-500 border-solid border-2
-      font-bold py-5 px-4 w-1/12 place-self-center mt-auto rounded-xl bg-slate-900' onClick={() => setregisterForm(!registerForm)}>
+      <button className='hover:bg-emerald-600 text-white md:text-xl border-slate-500 border-solid border-2
+      font-bold py-5 px-4 w-auto place-self-center mt-auto rounded-xl bg-slate-900' onClick={() => setregisterForm(!registerForm)}>
         {registerForm ? 'Login' : 'Sign in'}
       </button>
       {registerForm
         ? <RegisterForm loginUser={loginUser} setregisterForm={setregisterForm} />
         :
         <>      <span className='text-white font-bold text-2xl flex justify-center mb-10'>Login</span>
-          <div className='h-auto flex flex-col md:mx-36 relative bg-slate-800 w-auto px-16 py-12 rounded-3xl lg:mx-64 xl:mx-72 2xl:mx-96 grid-rows-3'>
-            <input onInput={() => setLoginError('')} className='bg-slate-900 w-4/5 place-self-center mt-5 text-center text-white rounded-xl border-solid border-2 border-slate-500 font-bold py-4 px-5 text-xl' type={"text"} onChange={(e) => setUsername(e.target.value)} value={username} placeholder='Username'></input>
-            <input onInput={() => setLoginError('')} className='bg-slate-900 mt-5 w-4/5 place-self-center text-center  text-white rounded-xl border-solid border-2 border-slate-500  font-bold py-4 px-5 text-xl' type={"password"} onChange={(e) => setPassword(e.target.value)} value={password} placeholder='Password'></input>
+          <form autoComplete='on' className='h-auto flex flex-col md:mx-36 relative bg-slate-800 w-auto px-16 py-12 rounded-3xl lg:mx-64 xl:mx-72 2xl:mx-96 grid-rows-3'>
+            <input autoFocus autoComplete="username" onInput={() => setLoginError('')} className='bg-slate-900 md:w-4/5 place-self-center mt-5 text-center text-white rounded-xl border-solid border-2 border-slate-500 font-bold py-4 px-5 md:text-xl' type={"text"} onChange={(e) => setUsername(e.target.value)} value={username} placeholder='Username'></input>
+            <input autoComplete="password" onInput={() => setLoginError('')} className='bg-slate-900 mt-5 md:w-4/5 place-self-center text-center  text-white rounded-xl border-solid border-2 border-slate-500  font-bold py-4 px-5 md:text-xl' type={"password"} onChange={(e) => setPassword(e.target.value)} value={password} placeholder='Password'></input>
             <span className='place-self-center my-8 text-red-500'>{loginError}</span>
-            <button className='hover:bg-emerald-600 text-white text-xl border-slate-500 border-solid border-2
-      font-bold py-5 px-4 w-2/5 place-self-center mt-auto rounded-xl bg-slate-900' type='button' onClick={loginButtonClick}>Login</button>
-          </div>
+            <button className='hover:bg-emerald-600 text-white md:text-xl border-slate-500 border-solid border-2
+      font-bold py-5 px-4 md:w-2/5 place-self-center mt-auto rounded-xl bg-slate-900' type='button' onClick={loginButtonClick} >Login</button>
+          </form>
         </>
       }
     </div>
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -36,14 +36,14 @@ function RegisterForm(props) {
   return (
     <>
       <span className='text-white font-bold text-2xl flex justify-center mb-10'>Sign in</span>
-      <div className='h-auto flex flex-col md:mx-36 relative bg-slate-800 w-auto px-16 py-12 rounded-3xl lg:mx-64 xl:mx-72 2xl:mx-96 grid-rows-3'>
-        <input onChange={(e) => setUsername(e.target.value)} className='bg-slate-900 w-4/5 place-self-center mt-5 text-center text-white rounded-xl border-solid border-2 border-slate-500 font-bold py-4 px-5 text-xl' type={"text"} value={username} placeholder='Username'></input>
-        <input onChange={(e) => setPassword1(e.target.value)} className='bg-slate-900 mt-5 w-4/5 place-self-center text-center  text-white rounded-xl border-solid border-2 border-slate-500  font-bold py-4 px-5 text-xl' type={"password"} value={password1} placeholder='Password'></input>
-        <input onChange={(e) => setPassword2(e.target.value)} className='bg-slate-900 mt-5 w-4/5 place-self-center text-center  text-white rounded-xl border-solid border-2 border-slate-500  font-bold py-4 px-5 text-xl' type={"password"} value={password2} placeholder='Repeat password'></input>
+      <form autoComplete='on' className='h-auto flex flex-col md:mx-36 relative bg-slate-800 w-auto px-16 py-12 rounded-3xl lg:mx-64 xl:mx-72 2xl:mx-96 grid-rows-3'>
+        <input autoComplete='username' onChange={(e) => setUsername(e.target.value)} className='bg-slate-900 md:w-4/5 w-auto place-self-center mt-5 text-center text-white rounded-xl border-solid border-2 border-slate-500 font-bold py-4 px-5 md:text-xl' type={"text"} value={username} placeholder='Username'></input>
+        <input autoComplete='new-password' onChange={(e) => setPassword1(e.target.value)} className='bg-slate-900 md:w-4/5 w-auto mt-5 place-self-center text-center  text-white rounded-xl border-solid border-2 border-slate-500  font-bold py-4 px-5 md:text-xl' type={"password"} value={password1} placeholder='Password'></input>
+        <input autoComplete='new-password' onChange={(e) => setPassword2(e.target.value)} className='bg-slate-900 md:w-4/5 w-auto mt-5 place-self-center text-center  text-white rounded-xl border-solid border-2 border-slate-500  font-bold py-4 px-5 md:text-xl' type={"password"} value={password2} placeholder='Repeat password'></input>
         <span className='place-self-center my-11 text-red-500'>{signInError}</span>
-        <button onClick={signInButtonClick} className='enabled:hover:bg-emerald-600 text-white text-xl border-slate-500 border-solid border-2
-      font-bold py-5 px-4 w-2/5 place-self-center mt-auto rounded-xl bg-slate-900' type='button'>Sign in</button>
-      </div>
+        <button onClick={signInButtonClick} className='enabled:hover:bg-emerald-600 text-white md:text-xl border-slate-500 border-solid border-2
+      font-semibold py-5 px-4 w-auto place-self-center mt-auto rounded-xl bg-slate-900' type='button'>Sign in</button>
+      </form>
     </>
   )
 }
